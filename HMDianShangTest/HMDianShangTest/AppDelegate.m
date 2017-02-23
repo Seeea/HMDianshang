9a59ad8d7fc2dc7d79e0297c2c0dec5ed74010d0
月夜雨墨 <seeea@seeeademacbook-pro.local>
2017-02-23 19:07:16 +0800

    ...

--- HMDianShangTest/HMDianShangTest/AppDelegate.m
+++ HMDianShangTest/HMDianShangTest/AppDelegate.m
@@ -18,6 +18,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     self.window = [[UIWindow alloc]initWithFrame:[UIScreen mainScreen].bounds];
     
+    //
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     MyTabbarController *tabbarCtr = [[MyTabbarController alloc]init];
